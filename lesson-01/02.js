708b6e38d415a61befe1490f2e2014f2483ad22c
AndreyUrazaev <f.a.z.a@bk.ru>
2023-09-19 16:09:10 +0500

    JS_homework_1_02(4)

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,17 +1,6 @@
 let number1 = Math.floor(Math.random() * 100);
-
-even = (number1%2 === 0);
-
-if(even === true){
+if(number1%2 === 0){
     console.log('«' + number1 + ' :Это четное число»');
 } else{
     console.log('«' + number1 + ' :Это нечетное число»');
 }
-
-// let number1 = Math.floor(Math.random() * 100);
-//
-// if(number1%2 === 0){
-//     console.log('«' + number1 + ' :Это четное число»');
-// } else{
-//     console.log('«' + number1 + ' :Это нечетное число»');
-// }
