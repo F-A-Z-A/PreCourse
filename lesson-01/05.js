1ad848cdba7764bb87cb7d96894d4b94cda177bd
AndreyUrazaev <f.a.z.a@bk.ru>
2023-09-23 13:42:17 +0500

    JS_homework_1_05(5)

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,13 +1,16 @@
 let rule = ["Еще не родился тот человек, который, поставил бы цель, и не смог бы стать программистом.", "Но если ты лентяй, то...", "Если стараешься не на 100"];
 rule = rule[Math.floor(Math.random() * 3)];
 switch (true) {
-    case rule.length < 25:
+    case rule.length < 25: {
         console.log("Все таки нет правил без исключения");
         break;
-    case rule.length > 25:
+    }
+    case rule.length > 25: {
         console.log("У меня 100 пудов все получится");
         break;
-    case rule.length === 25:
+    }
+    case rule.length === 25: {
         console.log("50 на 50");
         break;
+    }
 }
