bf4424f5a4bf6cd706c31f4b0e81c70b37b1ed67
AndreyUrazaev <f.a.z.a@bk.ru>
2023-10-20 12:15:47 +0500

    JS_homework_2_07(2)

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -6,7 +6,14 @@ let passportMarried = {
         city: "Bobryisk"
     }
 };
-let passportMarried2 = structuredClone(passportMarried);
-passportMarried2["married"] = true;
+let passportMarried2 = {
+    name: "Petr",
+    married: true,
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
 console.log(passportMarried);
 console.log(passportMarried2);
